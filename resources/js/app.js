920a31d166fba48f18d69e423cd4ed0c31e8da20
Donald Pakkies <donaldpakkies@gmail.com>
2019-01-30 18:31:25 +0200

    Feat: added a progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,11 @@ require('./bootstrap');
 // Dependencies
 import VueRouter from 'vue-router';
 import routes from './routes.js';
+import NProgress from 'nprogress';
+import { loadProgressBar } from 'axios-progress-bar';
+import '../../node_modules/nprogress/nprogress.css';
+
+loadProgressBar();
 
 // Components
 Vue.component('example-component', require('./components/example.vue'));
@@ -12,6 +17,18 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: 'history',
   routes: routes
+});
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+    NProgress.start();
+  }
+
+  next();
+});
+
+router.afterEach((to, from) => {
+  NProgress.done();
 });
 
 const app = new Vue({
