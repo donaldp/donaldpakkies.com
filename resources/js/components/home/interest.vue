4e46865506aac6aca8feec404026864e5b8a0739
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-06 20:11:31 +0200

    Feat: created a interest component

---
+++ resources/js/components/home/interest.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <section class="section-wrapper section-interest gray-bg">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-title">
+              <h2>Interest</h2>
+            </div>
+          </div>
+        </div>
+        <!-- .row -->
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="content-item">
+              <h3>Shows/Series</h3>
+
+              <p>Game of Thrones, Dexter, The Walking Dead, Limitless, Shameless, Brooklyn Nine-Nine.</p>
+            </div>
+          </div>
+        </div>
+        <!-- .row -->
+
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.');
+    }
+  }
+
+</script>
