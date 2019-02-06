483cdc1f4e83e806b9c90a477f793f0ccaa5711c
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-06 20:11:20 +0200

    Feat: created a intro component

---
+++ resources/js/components/home/intro.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="intro section-wrapper">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-title">
+              <h2>What I am all about.</h2>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <p>
+              Just a random 21 year old who is trying to build cool sh*t. I'm a very innovative Software
+              Engineer who spent his entire childhood writing code. I like coming up with new things (like building
+              a new Programming Language or creating a Web Framework) and learning new stacks.
+
+            </p>
+
+            <p>
+              I'm a Open Source contributor and an advocate for good quality Software. If i tell you "i don't like
+              your code", It's probably because i think you can improve :).
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.');
+    }
+  }
+</script>
