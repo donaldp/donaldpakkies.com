c2e6e548ebded467610a4272b16a4cc713247b7a
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-06 20:12:06 +0200

    Feat: created a index component

---
+++ resources/js/components/home/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="right-col-block blocks">
+      <intro></intro>
+      <portfolio></portfolio>
+      <education></education>
+      <interest></interest>
+
+      <footer class="footer">
+        <div class="copyright-section">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="copytext">&copy; 2019 Donald Pakkies. All rights reserved | Design By: <a href="https://themehippo.com">ThemeHippo</a></div>
+              </div>
+            </div>
+            <!--.row-->
+          </div>
+          <!-- .container-fluid -->
+        </div>
+        <!-- .copyright-section -->
+      </footer>
+      <!-- .footer -->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.');
+    }
+  }
+</script>
