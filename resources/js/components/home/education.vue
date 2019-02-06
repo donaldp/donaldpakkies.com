9930e9d7810ff4849ee6e280b4d3e3394d01a92a
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-06 20:11:02 +0200

    Feat: created a education component

---
+++ resources/js/components/home/education.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section class="section-wrapper section-education">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-title">
+              <h2>Education</h2>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="content-item">
+              <small>2017 - NA</small>
+              <h3>MCSD, Intro to Programming</h3>
+              <h4>Boston City College & Business College</h4>
+
+              <p>South Africa, Pretoria</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.');
+    }
+  }
+
+</script>
