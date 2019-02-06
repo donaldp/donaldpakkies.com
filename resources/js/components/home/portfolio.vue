45e66948f21644a9424941df6bce79922e545563
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-06 20:11:51 +0200

    Feat: created a portfolio component

---
+++ resources/js/components/home/portfolio.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <section class="section-wrapper section-experience gray-bg">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-title">
+              <h2>Portfolio</h2>
+            </div>
+          </div>
+        </div>
+        <!--.row-->
+        <div class="row">
+          <div class="col-md-12">
+            <div class="content-item">
+              <small>2013 - 2017</small>
+              <h3>Xylon Studio</h3>
+              <h4>IDE for C% Windows Applications and JavaScript based Windows Applications.</h4>
+
+              <p><a href="http://www.xylonstudio.com">xylonstudio.com</a></p>
+            </div>
+            <!-- .experience-item -->
+            <div class="content-item">
+              <small>2017 - 2017</small>
+              <h3>C Modulus</h3>
+              <h4>A Programming Language for Software and Web Development.</h4>
+
+              <p><a href="http://cmodulus.xylonstudio.com">cmodulus.xylonstudio.com</a></p>
+            </div>
+            <!-- .experience-item -->
+            <div class="content-item">
+              <small>2018 - Present</small>
+              <h3>Modulus</h3>
+              <h4>A cool API framework for PHP.</h4>
+
+              <p><a href="https://modulusphp.com">modulusphp.com</a></p>
+            </div>
+            <!-- .experience-item -->
+            <div class="content-item">
+              <small>2018 - Present</small>
+              <h3>Delivr</h3>
+              <h4>Email delivery service for Developers</h4>
+
+              <p><a href="https://delivr.modulusphp.com">delivr.modulusphp.com</a></p>
+            </div>
+            <!-- .experience-item -->
+          </div>
+        </div>
+        <!--.row-->
+      </div>
+      <!-- .container-fluid -->
+
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.');
+    }
+  }
+</script>
