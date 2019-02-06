fbae58086256dcda572b8acd16f67026a268b94c
Donald Pakkies <donaldpakkies@gmail.com>
2019-02-06 20:09:22 +0200

    Feat: added a config for social links

---
+++ config/social.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Social networks
+ */
+return [
+  'twitter' => 'https://twitter.com/donaldpakkies',
+  'linkedin' => 'https://linkedin.com/in/donaldpakkies',
+  'github' => 'https://github.com/donaldp'
+];
