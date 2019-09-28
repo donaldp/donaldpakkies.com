9c54c6e738f8990004a5ead09fcaa2ffa0d9601d
Donald Pakkies <donald@sov.tech>
2019-09-28 23:31:26 +0200

    ref: load components by including default

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,12 +10,12 @@ import '../../node_modules/nprogress/nprogress.css';
 loadProgressBar();
 
 // Components
-Vue.component('example-component', require('./components/example.vue'));
-Vue.component('home-view', require('./components/home/index.vue'));
-Vue.component('intro', require('./components/home/intro.vue'));
-Vue.component('portfolio', require('./components/home/portfolio.vue'));
-Vue.component('education', require('./components/home/education.vue'));
-Vue.component('interest', require('./components/home/interest.vue'));
+Vue.component('example-component', require('./components/example.vue').default);
+Vue.component('home-view', require('./components/home/index.vue').default);
+Vue.component('intro', require('./components/home/intro.vue').default);
+Vue.component('portfolio', require('./components/home/portfolio.vue').default);
+Vue.component('education', require('./components/home/education.vue').default);
+Vue.component('interest', require('./components/home/interest.vue').default);
 
 Vue.use(VueRouter);
 
