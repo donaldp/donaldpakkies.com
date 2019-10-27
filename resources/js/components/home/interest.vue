00ab1cf6f1a16a56996e97e977d6f31a3e03cfe1
Donald Pakkies <donald@sov.tech>
2019-10-27 19:33:16 +0200

    ref: updated header

--- resources/js/components/home/interest.vue
+++ resources/js/components/home/interest.vue
@@ -5,7 +5,7 @@
         <div class="row">
           <div class="col-md-12">
             <div class="section-title">
-              <h2>Interest</h2>
+              <h2>Interests</h2>
             </div>
           </div>
         </div>
