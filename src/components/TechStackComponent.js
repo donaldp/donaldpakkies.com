f8776ae2e47f95f9c389e7ac766dfa741e2224a6
Donald Pakkies <donaldpakkies@gmail.com>
2020-07-01 17:04:07 +0200

    feat: added my techstack

---
+++ src/components/TechStackComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TechStackComponent = () => {
+  return (
+    <section className="section-wrapper section-inter">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="section-title">
+              <h2>Stack</h2>
+            </div>
+          </div>
+        </div>
+
+        <a data-theme="light" data-layers="1,2,3,4" data-stack-embed="true" href="https://embed.stackshare.io/stacks/embed/dc344aa5fc314321d9c9fd694b4dc4">See Tech Stack</a>
+      </div>
+    </section>
+  )
+};
+
+export default TechStackComponent;
