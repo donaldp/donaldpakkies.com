e83243e7f3ffcdc4b204936a7756fd249660c660
Loki <donaldpakkies@gmail.com>
2021-03-10 08:44:18 +0200

    refactor: update interests

--- src/components/InterestsComponent.js
+++ src/components/InterestsComponent.js
@@ -17,19 +17,19 @@ const InterestsComponent = (props) => {
             <div className="content-item">
               <h3>Shows/Series</h3>
 
-              <p>Ozark, Lucifer, Grimm, Game of Thrones, Dexter, The Walking Dead, Limitless, Shameless, Brooklyn Nine-Nine, Mr Iglesias, StarGirl, Space Force, Gotham, The Order, Warrior Nun.</p>
+              <p>WandaVision, Ozark, Lucifer, Grimm, Game of Thrones, Dexter, The Walking Dead, Limitless, Shameless, Brooklyn Nine-Nine, Mr Iglesias, StarGirl, Space Force, Gotham, The Order, Warrior Nun.</p>
             </div>
 
             <div className="content-item">
               <h3>Games</h3>
 
-              <p>Fortnite, FIFA, Grand Theft Auto, Red Dead Redemption, Forza, Assassin's Creed, Far Cry, Call of Duty: Modern Warfare.</p>
+              <p>Star Wars Jedi: Fallen Order, Fortnite, FIFA, Grand Theft Auto Online, Red Dead Redemption, Forza, Assassin's Creed, Far Cry, Call of Duty: Modern Warfare.</p>
             </div>
 
             <div className="content-item">
               <h3>Music</h3>
 
-              <p>Queen, Guns N' Roses, NAV, AC/DC, Drake, Phora, A-Reece, Travis Scott, Logic, Majid Jordan, La Paris, Mura Masa, Russ, Monxx, The Weeknd.</p>
+              <p>Queens, Guns N' Roses, NAV, AC/DC, Drake, Phora, A-Reece, Travis Scott, Logic, Majid Jordan, La Paris, Mura Masa, Russ, Monxx, The Weeknd.</p>
             </div>
 
             <div className="content-item">
