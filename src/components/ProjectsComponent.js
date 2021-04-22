85d7d32e7dc0010ba3edde194154f5dee6a2e99a
Donald Pakkies <donaldpakkies@gmail.com>
2021-04-22 21:36:13 +0200

    feat: added projects worked on

---
+++ src/components/ProjectsComponent.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const ProjectsComponent = (props) => {
+  return (
+    <section className="section-wrapper gray-bg" style={{ borderTop: '1px solid #cecece' }}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="section-title">
+              <h2>Projects</h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="expertise-item">
+              <h3><a href="http://github.com/harnessflex">Harness</a></h3>
+              <h4>MVC Framework for Imba Lang</h4>
+
+              <p>Supports Auth, Laravel Mix, Vuejs, Liquidjs, Localization, DB Migrations, and more</p>
+
+              <small>March 2021</small>
+            </div>
+
+            <div className="expertise-item">
+              <h3><a href="http://github.com/harnessflex/schema-builder">SQL Schema Builder</a></h3>
+              <h4>SQL Schema Builder</h4>
+
+              <p>Provides a cleaner syntax for db-migrate (inspired by Laravel Eloquent)</p>
+
+              <small>March 2021</small>
+            </div>
+
+            <div className="expertise-item">
+              <h3><a href="http://github.com/modulusphp">Modulus</a></h3>
+              <h4>MVC Framework for PHP</h4>
+
+              <p>Supports Auth, Laravel Mix, Vuejs / Reactjs, Queue workers, AWS integration, DB Migrations, and more</p>
+
+              <small>Jan 2018</small>
+            </div>
+
+            <div className="expertise-item">
+              <h3><a href="https://github.com/donaldp/xylon-studio-ide">Xylon Studio</a></h3>
+              <h4>Integrated Development Environment for Windows</h4>
+
+              <small>2013</small>
+            </div>
+
+            <div className="expertise-item">
+              <h3>C Modulus</h3>
+              <h4>A Programming Language that compiles to JavaScript</h4>
+
+              <small>2016</small>
+            </div>
+
+            <div className="expertise-item">
+              <h3>Audiostreamify</h3>
+              <h4>A music streaming service writting in C Modulus, JavaScript & PHP</h4>
+
+              <small>2017</small>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  )
+}
+
+export default ProjectsComponent;
--- src/components/WorkComponent.js
+++ src/components/WorkComponent.js
@@ -17,7 +17,57 @@ const WorkComponent = (props) => {
             <div className="expertise-item">
               <h3><a href="https://sov.tech">SovTech</a></h3>
               <h4>Full Stack Lead Developer</h4>
+
+              <div className="row" style={{ paddingLeft: '20px' }}>
+                <div className="col-md-12">
+                   <div className="expertise-item">
+                    <strong><a href="https://www.fincheck.co.za/">Fincheck / MyFincheck</a></strong>
+
+                    <br/>
+                    Lead Backend Developer - API Development, Payment Gatway & Credit Bureau API's Integration, DevOps, Code Reviews
+                  </div>
+
+                  <div className="expertise-item">
+                    <strong><a href="https://reachforadream.org.za/">Reach for a Dream</a></strong>
+
+                    <br/>
+                    Full Stack Developer - Maintenance & Bug fixes
+                  </div>
+
+                  <div className="expertise-item">
+                    <strong><a href="https://www.yalu.co.za/">Yalu</a></strong>
+
+                    <br/>
+                    Full Stack Developer - Maintenance & Bug fixes, Code Reviews
+                  </div>
+                </div>
+              </div>
+
               <small>Jan 2018 - Jan 2020</small>
+
+              <p>South Africa, Randburg</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="expertise-item">
+              <h3><a href="https://www.prepaidmeters.co.za/">Prepaid Meters</a></h3>
+              <h4>Backend Developer</h4>
+
+              <div className="row" style={{ paddingLeft: '20px' }}>
+                <div className="col-md-12">
+                   <div className="expertise-item">
+                    <strong>MVC Framework</strong>
+
+                    <br/>
+                    Backend Development - Custom web Framework Development
+                  </div>
+                </div>
+              </div>
 
               <p>South Africa, Randburg</p>
             </div>
@@ -32,7 +82,32 @@ const WorkComponent = (props) => {
               <h3><a href="https://donaldpakkies.com">Freelancing</a></h3>
               <h4>Full Stack Developer</h4>
 
-              <small>Jan 2020 - current</small>
+              <div className="row" style={{ paddingLeft: '20px' }}>
+                <div className="col-md-12">
+                   <div className="expertise-item">
+                    <strong>Financial Bunny Gym Chatbot</strong> @ SmartMacmane
+
+                    <br/>
+                    Full Stack Developer - Twilio Integration, Chat Flow & Backend / Dashboard Development, DevOps
+                  </div>
+
+                  <div className="expertise-item">
+                    <strong><a href="https://spotmoney.com">Spot Money</a></strong> @ WiGroup
+
+                    <br/>
+                    Frontend Developer - Offers Components & Dashboard Development
+                  </div>
+
+                  <div className="expertise-item">
+                    <strong><a href="https://www.weareegg.co.za/">We are EGG</a></strong> @ LT
+
+                    <br/>
+                    Lead Backend Developer - Backend / Portal & API Development, SQS & Xero Integration, DevOps
+                  </div>
+                </div>
+              </div>
+
+              <small>current</small>
               <p>South Africa</p>
             </div>
           </div>
